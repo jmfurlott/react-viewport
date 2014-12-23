a1b5be378f14f98dd9cbd22023a6c926adac97b5
Joseph Furlott <jmfurlott@gmail.com>
2014-12-23 10:34:12 -0500

    classes

--- src/ViewportContainer.js
+++ src/ViewportContainer.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import React from 'react/addons';
+var cx = React.addons.classSet;
+
 
 let ViewportContainer = React.createClass({ 
 
@@ -12,12 +14,22 @@ let ViewportContainer = React.createClass({
   },
 
   render() {
+   
+    let classes = [ 'ViewportContainer' ];
+
+    if(!!this.props.className) {
+      classes.push(this.props.className);
+    }
+
+    classes = classes.join(' ');
+
+    console.log(this.props.viewHeight);
+
     return(
-      <div className={"ViewportContainer " + this.props.className}
+      <div 
         {...this.props}
-      >
-        Hello world
-      </div>
+        className={classes}
+      />
     );
   }
 });
